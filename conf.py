58f2f7383f6dcc0a2baf13d404d87423580034bc
Olivier Bonaventure <Olivier.Bonaventure@uclouvain.be>
2013-01-31 23:04:44 +0100

    Changement mineur

--- conf.py
+++ conf.py
@@ -75,7 +75,7 @@ language = 'fr'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '.#*', '*/.#*', '*qcm*.rst' ]
+exclude_patterns = ['_build', '.#*', '*/.#*', 'Exercices/QCM/*' ]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
