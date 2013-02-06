c2aec7a8cd973c6b5e9294ea36a639601c2a5085
Gregory Detal <gregory.detal@uclouvain.be>
2013-02-06 11:04:16 +0100

    Application des commentaires de Florian
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- Exercices/Projets/bitstring.h
+++ Exercices/Projets/bitstring.h
@@ -3,26 +3,28 @@
 
 /* /!\ Fichier à ne pas changer */
 
+#include <stdlib.h>
+
 struct bitstring;
 /* A vous de définir votre structure dans bitstring.c */
 
 typedef struct bitstring bitstring_t;
 typedef unsigned char bit_t;
 
-/* Alloue une bitstring de <code>n</code> bits initialisés à 0 */
+/* Alloue un bitstring de <code>n</code> bits initialisés à 0 */
 bitstring_t *bitstring_alloc(size_t n);
 
 /* Met le <code>n</code>ième bit à la valeur de <code>bit</code>
  * du bitstring <code>b</code>
  *
- * <code>n</code> est plus grand ou égale à 0 et strictement plus petit que 
+ * <code>n</code> est plus grand ou égal à 0 et strictement plus petit que 
  * <code>bitstring_len(b)</code>
  */
 void bitstring_set(bitstring_t *b, unsigned int n, bit_t bit);
 
 /* Retourne la valeur du <code>n</code>ième bit du bitstring <code>b</code>
  *
- * <code>n</code> est plus grand ou égale à 0 et strictement plus petit que 
+ * <code>n</code> est plus grand ou égal à 0 et strictement plus petit que 
  * <code>bitstring_len(b)</code>
  */
 bit_t bitstring_get(bitstring_t *b, int n);
@@ -50,19 +52,20 @@ void bitstring_shift(bitstring_t *b, int n);
 /* Effectue l'opération ou exclusif entre <code>b1</code> et <code>b2</code> 
  * (<code>b1 ^ b2</code>).
  *
- * Le résultat est fourni à l'appellant via <code>res</code>.
+ * Si les bitstring <code>b1</code> et <code>b2</code> sont de taille
+ * différente, une erreur est renvoyée. Sinon, le résultat est fourni à
+ * l'appellant via <code>res</code>. <code>b1</code> et <code>b2</code>
+ * sont inchangés.
  * La valeur de retour est 0 en cas de succès, -1 en cas d'erreur.
  */
 int bitstring_xor(bitstring_t *b1, bitstring_t *b2, bitstring_t **res);
 
 /* Affiche en représentation hexadécimale le bitstring <code>b</code> dans
- * le buffer <code>buf</code> de taille <code>*len</code>. Retourne 
+ * le buffer <code>buf</code> de taille <code>len</code>. Retourne 
  * <code>-1</code> si le buffer ne peut contenir toute la representation.
- * Retourne <code>0</code> sinon. <code>len</code> est mis à jour pour valoir
- * le nombre de caractères hexadécimals affiché dans <code>buf</code> + le
- * caractère '\0'.
+ * Retourne le nombre de caractères hexadécimals (+'\0') écrits sinon. 
  */
-int bitstring_print(bitstring_t *b, char *buf, size_t *len);
+int bitstring_print(bitstring_t *b, char *buf, size_t len);
 
 /* Libère la mémoire du bitstring */
 void bitstring_free(bitstring_t *b);
